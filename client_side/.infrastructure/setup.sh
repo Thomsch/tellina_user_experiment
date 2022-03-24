f9a4bb94df42fdb1e3942be6d07023f119eee939
Thomas Schweizer <tschweiz@cs.washington.edu>
2022-03-24 16:19:41 -0700

    Add space between command and description

--- client_side/.infrastructure/setup.sh
+++ client_side/.infrastructure/setup.sh
@@ -124,9 +124,9 @@ alias expected='show_expected; touch ${INFRA_DIR}/.noverify'
 show_help() {
   echo "--- Help -----------------------------------------------------------------------";
   echo "Available commands:";
-  echo "expected shows a diff with the expected result.";
-  echo "skip     gives up on the current task and starts the next task.";
-  echo "helpme   prints this help message.";
+  echo "expected    shows a diff with the expected result.";
+  echo "skip        gives up on the current task and starts the next task.";
+  echo "helpme      prints this help message.";
   echo ""
 }
 
